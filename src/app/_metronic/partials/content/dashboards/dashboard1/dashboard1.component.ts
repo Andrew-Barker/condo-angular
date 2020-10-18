d506d2b43aa33d688279ad0e2c53bf258722a646
Barker, Andrew S <andrew.barker@kroger.com>
2020-10-17 21:36:58 -0400

    added weather card to UI

--- src/app/_metronic/partials/content/dashboards/dashboard1/dashboard1.component.ts
+++ src/app/_metronic/partials/content/dashboards/dashboard1/dashboard1.component.ts
@@ -10,6 +10,7 @@ import { ViewChild } from '@angular/core';
 @Component({
   selector: 'app-dashboard1',
   templateUrl: './dashboard1.component.html',
+  styleUrls: ['./dashboard1.component.scss']
 })
 export class Dashboard1Component implements OnInit {
 
