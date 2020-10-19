17a84208666c052643cb927b73a3001741ee45a3
Barker, Andrew S <andrew.barker@kroger.com>
2020-10-18 23:57:57 -0400

    add icons for poi to map

--- src/app/modules/maps/poi/poi.component.ts
+++ src/app/modules/maps/poi/poi.component.ts
@@ -10,18 +10,44 @@ import { MapsService } from '../maps.service';
 export class PoiComponent implements OnInit {
   lat= 27.3511812;
   lng = -82.6093296;
+  condoIcon = "./assets/media/maps/condo_marker.png";
+  flatPOI = [];
   pointsOfInterest = [];
 
-  constructor(private mapService: MapsService, private changeDetection: ChangeDetectorRef) {
-    console.log('poi constructor');
-   }
+  constructor(private mapService: MapsService, private changeDetection: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     console.log('before poi rest call');
     this.mapService.getPointsOfInterest().subscribe(poiResp => {
       this.pointsOfInterest = poiResp;
       this.changeDetection.detectChanges();
-      console.log('poi', poiResp);
+
+      console.log(poiResp);
+
+      this.pointsOfInterest.forEach(category => {
+        category.places.forEach(place => {
+          let icon: string;
+          
+          switch(category.category){
+            case 'Restaurants':
+              icon = "./assets/media/maps/restaurant.png"
+              break;
+            case 'Grocery':
+              icon = "./assets/media/maps/grocery.png"
+              break;
+            default:
+              icon = "./assets/media/maps/attraction.png"
+              break;
+          }
+
+          place['icon'] = icon; 
+          this.flatPOI.push(place);
+        })
+      });
+
+      console.log('flatPOI', this.flatPOI);
+      this.changeDetection.detectChanges();
+
     }, error => {
       console.error('error on get poi', error);
     })
